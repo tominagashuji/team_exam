5aa756d2101562efebea78e63e554134b07bb9e8
shujitominagadownload <shuji_tominaga@diveintocode.jp>
2019-02-25 21:00:24 +0900

    権限移動機能を追加

--- app/helpers/team_helper.rb
+++ app/helpers/team_helper.rb
@@ -2,4 +2,8 @@ module TeamHelper
   def default_img(image)
     image.presence || 'default.jpg'
   end
+
+  def owner?(assign)
+    current_user.id == assign.team.owner_id && !(current_user.id == assign.user.id)
+  end
 end
